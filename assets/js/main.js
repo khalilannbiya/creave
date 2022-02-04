c128d9dd1f40f941d2377432bf7e2cdeb7ba6ed6
khalilannbiya <syeichkhalil30@gmail.com>
2022-02-04 08:14:05 +0700

    update responsive swiper

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,28 @@
 const swiper = new Swiper(".swiper", {
    loop: true,
-   slidesPerView: 5,
-   spaceBetween: 20,
+   slidesPerView: 2,
+   spaceBetween: 10,
    centeredSlides: true,
    grabCursor: true,
    autoplay: {
       delay: 4000,
+   },
+   breakpoints: {
+      312: {
+         slidesPerView: 2,
+         spaceBetween: 10,
+         centeredSlides: true,
+      },
+      662: {
+         slidesPerView: 3,
+         spaceBetween: 15,
+         centeredSlides: true,
+      },
+      992: {
+         slidesPerView: 5,
+         spaceBetween: 15,
+         centeredSlides: true,
+      },
    },
 
    // If we need pagination
