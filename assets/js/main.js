e78f0d7f3f784fb974b0ff15a4fe48fee6b6210d
ramaherdianto <ramadhanherdianto35@gmail.com>
2022-02-05 04:12:07 +0700

    Menambahkan scrollbar dan preloader. Section OurTeam sudah selesai dan responsive

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,39 +1,55 @@
+// =========== Preloader
+(function ($) {
+  "use strict";
+
+  // Preloader
+  $(window).on("load", function () {
+    if ($("#preloader").length) {
+      $("#preloader")
+        .delay(100)
+        .fadeOut("slow", function () {
+          $(this).remove();
+        });
+    }
+  });
+})(jQuery);
+
 const swiper = new Swiper(".swiper", {
-   loop: true,
-   slidesPerView: 2,
-   spaceBetween: 10,
-   centeredSlides: true,
-   grabCursor: true,
-   autoplay: {
-      delay: 4000,
-   },
-   breakpoints: {
-      312: {
-         slidesPerView: 2,
-         spaceBetween: 10,
-         centeredSlides: true,
-      },
-      662: {
-         slidesPerView: 3,
-         spaceBetween: 15,
-         centeredSlides: true,
-      },
-      992: {
-         slidesPerView: 5,
-         spaceBetween: 15,
-         centeredSlides: true,
-      },
-   },
+  loop: true,
+  slidesPerView: 2,
+  spaceBetween: 10,
+  centeredSlides: true,
+  grabCursor: true,
+  autoplay: {
+    delay: 4000,
+  },
+  breakpoints: {
+    312: {
+      slidesPerView: 2,
+      spaceBetween: 10,
+      centeredSlides: true,
+    },
+    662: {
+      slidesPerView: 3,
+      spaceBetween: 15,
+      centeredSlides: true,
+    },
+    992: {
+      slidesPerView: 5,
+      spaceBetween: 15,
+      centeredSlides: true,
+    },
+  },
 
-   // If we need pagination
-   pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-   },
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
 
-   // Navigation arrows
-   navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-   },
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
 });
